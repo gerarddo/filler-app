de386281ef04eddfdc7621fa9faea9263b8383b6
gerarddo <germijares280995@hotmail.com>
2018-02-16 16:00:33 -0600

    same

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ app.get('*', (req, res) => {
 
 
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000,
-    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.PORT || 3000
 
-app.listen(port, ip, function() {
+app.listen(port, function() {
   console.log('Filler server listening on %d', port);
 });
